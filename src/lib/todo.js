29dab817f5b4f394c062a2f1e5a9ebb41d4b9cdd
Sidney Lemon <sid.lemon3@gmail.com>
2023-03-21 12:36:27 -0600

    initial

---
+++ src/lib/todo.js
@@ -0,0 +1,11 @@
+const tasks = {
+    first: "Get out of bed",
+    second: "Make coffee",
+    third: "Read feeds",
+    forth: "Have shower",
+    fifth: "Brush teeth",
+    sixth: "Get dressed",
+    seventh: "Leave for work",
+    eighth: "Get breakfast to go",
+  };
+  export const todo = [tasks];
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { todo } from "/src/lib/todo.js";
+</script>
+
+<main>
+  <h1>Achievement 19</h1>
+  <section>
+    {#each todo as todo}
+      <article class="card">
+        <ol>
+          <li>{todo.first}</li>
+          <li>{todo.second}</li>
+          <li>{todo.third}</li>
+          <li>{todo.forth}</li>
+          <li>{todo.fifth}</li>
+          <li>{todo.sixth}</li>
+          <li>{todo.seventh}</li>
+          <li>{todo.eighth}</li>
+        </ol>
+      </article>
+    {/each}
+  </section>
+</main>
